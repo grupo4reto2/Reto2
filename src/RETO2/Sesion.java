448c73567d228da443aafedf1ac287df0a498199
grupo4reto2 <grupo4githubreto2@gmail.com>
2026-01-12 04:03:39 +0100

    Update Sesion.java
    ...

--- src/RETO2/Sesion.java
+++ src/RETO2/Sesion.java
@@ -2,88 +2,82 @@ package RETO2;
 import java.time.LocalDateTime;
 public class Sesion {
 
-	private int id;
+	private int idSesion;
 	private LocalDateTime fechaInicio;
 	private LocalDateTime fechaFin;
-	private int sala ;
+	private Sala sala;
 	private double precio;
 	private String pelicula;
 	
-	
-	public Sesion (int id, LocalDateTime fechaInicio, LocalDateTime fechaFin, int sala, double precio, String pelicula) {
-		this.id = id;
+	public Sesion (int idSesion, LocalDateTime fechaInicio, LocalDateTime fechaFin, Sala sala, double precio, String pelicula) {
+		this.idSesion = idSesion;
 		this.fechaInicio = fechaInicio;
 		this.fechaFin = fechaFin;
 		this.sala = sala;
 		this.precio = precio;
 		this.pelicula = pelicula;
-	
-}
+		
+		}
 
-
-	public int getId() {
-		return id;
+	public String getPelicula() {
+		return pelicula;
 	}
 
-	
-	public void setId(int id) {
-		this.id = id;
+	public void setPelicula(String pelicula) {
+		this.pelicula = pelicula;
 	}
 
+	public int getIdSesion() {
+		return idSesion;
+	}
+
+	public void setIdSesion(int idSesion) {
+		this.idSesion = idSesion;
+	}
 
 	public LocalDateTime getFechaInicio() {
 		return fechaInicio;
 	}
 
-
 	public void setFechaInicio(LocalDateTime fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
-
 
 	public LocalDateTime getFechaFin() {
 		return fechaFin;
 	}
 
-
 	public void setFechaFin(LocalDateTime fechaFin) {
 		this.fechaFin = fechaFin;
 	}
 
-
-	public int getSala() {
+	public Sala getSala() {
 		return sala;
 	}
 
-
-	public void setSala(int sala) {
+	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
-
 
 	public double getPrecio() {
 		return precio;
 	}
-
 
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
 
 
-	public String getPelicula() {
-		return pelicula;
-	}
-
-
-	public void setPelicula(String pelicula) {
-		this.pelicula = pelicula;
-	}
 	
 	@Override
 	
 	public String toString() {
-		return "ID: " + id + " Inicio: " + fechaInicio + " Fin: " + fechaFin + " Sala: " + sala + " Precio: " + precio + " Película: " + pelicula;
+		return  "Sesion ID :" + idSesion +
+				" Inicio: " + fechaInicio +
+				" Fin: " + fechaFin +
+				" Sala: " + sala.getNombre() +
+				" Precio: " + precio +
+				" Película: " + pelicula;
 	}
 	
 }
