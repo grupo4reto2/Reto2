d6de600cf1f84756678679ff3a10a4115a77a61b
grupo4reto2 <grupo4githubreto2@gmail.com>
2026-01-16 17:11:21 +0100

    CompraUpdated.java
    añadido dniCliente y importe

--- src/RETO2/Compra.java
+++ src/RETO2/Compra.java
@@ -2,11 +2,7 @@ package RETO2;
 
 import java.time.LocalDateTime;
 
-
-
 public class Compra {
-
-
 
 	private int idCompra;
 
@@ -14,15 +10,16 @@ public class Compra {
 
 	private Cliente cliente;
 
-	private LocalDateTime fechaHora;
+	private  String dniCliente;
 
-	private double precio;
+	private LocalDateTime fechaHora;
 
 	private double descuento;
 
-	
+	private double importe;
 
-	public Compra (int idCompra, Entrada numEntradas, Cliente cliente, LocalDateTime fechaHora, double precio, double descuento) {
+	
+	public Compra (int idCompra, Entrada numEntradas, Cliente cliente, String dniCliente, LocalDateTime fechaHora, double precio, double descuento, double importe) {
 
 		this.idCompra = idCompra;
 
@@ -30,132 +27,105 @@ public class Compra {
 
 		this.cliente = cliente;
 
+		this.dniCliente = dniCliente
+
 		this.fechaHora = fechaHora;
 
-		this.precio = precio;
-
 		this.descuento = descuento;
+
+		this.importe = importe;
 
 	}
 
 	
-
-
 	public int getIdCompra() {
-
 		return idCompra;
 
 	}
 
-
-
 	public void setIdCompra(int idCompra) {
-
 		this.idCompra = idCompra;
 
 	}
 
-
-
 	public Entrada getNumEntradas() {
-
 		return numEntradas;
 
 	}
 
-
-
 	public void setNumEntradas(Entrada numEntradas) {
-
 		this.numEntradas = numEntradas;
 
 	}
 
-
-
 	public Cliente getCliente() {
-
 		return cliente;
 
 	}
 
-
-
 	public void setCliente(Cliente cliente) {
-
 		this.cliente = cliente;
 
 	}
 
+	public String getDNICliente() {
+		return cliente;
 
+	}
+
+	public void setDNICliente(String dniCliente) {
+		this.dniCliente = dniCliente;
+
+	}
 
 	public LocalDateTime getFechaHora() {
-
 		return fechaHora;
 
 	}
 
-
-
 	public void setFechaHora(LocalDateTime fechaHora) {
-
 		this.fechaHora = fechaHora;
 
 	}
 
-
-
 	public double getPrecio() {
-
 		return precio;
 
 	}
 
-
-
 	public void setPrecio(double precio) {
-
 		this.precio = precio;
 
 	}
 
-
-
 	public double getDescuento() {
-
 		return descuento;
-
 	}
-
-
-
-	public void setDescuento(double descuento) {
-
-		this.descuento = descuento;
-
-	}
-
 	
+	public void setDescuento(double descuento) {
+		this.descuento = descuento;
+	}
+
+	public double getImporte() {
+		return importe;
+	}
+	
+	public void setDescuento(double descuento) {
+		this.importe = importe;
+	}
 
 	@Override 
 
-	
-
 	public String toString() {
-	
-
+		
 		return "ID compra: " + idCompra +
-
 				" Número entradas: " + numEntradas.getNumEntradas() + 
-
-				" Cliente: " + cliente.getNombreApellidos() +
-
-				" Fecha y hora de la compra: " + fechaHora + 
-
-				" Descuento aplicado: " + descuento;
-
+				" Cliente: " + cliente.getNombre() + cliente.getApellido() +
+				" DNICliente: " + dniCliente.getdniCliente() +
+				" Fecha y hora de la compra: " + fechaHora.getFechaHora () +
+				" Importe de la compra: " + importe.getImporte ()
+				" Descuento aplicado: " + descuento.getDescuento ();
+				
 	}
-
-	
 
 }
