4c6d46afd30cba7ba0d24725338bf946124c4109
grupo4reto2 <grupo4githubreto2@gmail.com>
2026-01-21 16:11:17 +0100

    EntradaUpdated.java
    cambiado el nombre del objeto sesion a idSesion

--- src/model/Entrada.java
+++ src/model/Entrada.java
@@ -3,14 +3,15 @@ package model;
 public class Entrada {
 
 	private int idEntrada;
-	private Sesion sesion;
 	private int numPersonas;
+	private Sesion idSesion;
 	private Compra idCompra;
 	private LocalDateTime fecHoraIni;
-	public Entrada (int idEntrada, Sesion sesion, int numPersonas, Compra idCompra, LocalDateTime fecHoraIn) {
+	
+	public Entrada (int idEntrada, int numPersonas, Sesion idSesion Compra idCompra, LocalDateTime fecHoraIn) {
 		this.idEntrada = idEntrada;
-		this.sesion = sesion;
 		this.numPersonas = numPersonas;
+		this.idSesion = idSesion;
 		this.idCompra = idCompra;
 		this.fecHoraIni = fecHoraIni;
 	}
@@ -34,42 +35,25 @@ public class Entrada {
 	}
 
 
-
-
-	public Sesion getSesion() {
+	public Sesion getIdSesion() {
 		return sesion;
 	}
 
-
-
-
-	public void setSesion(Sesion sesion) {
+	public void setIdSesion(Sesion sesion) {
 		this.sesion = sesion;
 	}
-
-
-
 
 	public int getNumPersonas() {
 		return numPersonas;
 	}
 
-
-
-
 	public void setNumPersonas(int numPersonas) {
 		this.numPersonas = numPersonas;
 	}
 
-
-
-
 	public Compra getIdCompra() {
 		return idCompra;
 	}
-
-
-
 
 	public void setIdCompra(Compra idCompra) {
 		this.idCompra = idCompra;
@@ -83,9 +67,10 @@ public class Entrada {
 	public String toString() {
 		
 		return "ID entrada: " + idEntrada +
-			   " Sesión: " + sesion.getIdSesion() +
-			   " Número de personas: " + numPersonas + 
-				" ID de Compra: " + idCompra.getIdCompra();
+				" Número de personas: " + numPersonas + 
+			    " Sesión: " + sesion.getIdSesion() +
+				" ID de Compra: " + idCompra.getIdCompra() +
+				" Fecha de compra: " + fecHoraIni.getFecHoraIni();
 			 
 	}
 	
