b43d2a1efc3dfe7e1fe4fec6fdb4da8ee9678888
grupo4reto2 <grupo4githubreto2@gmail.com>
2026-01-21 15:40:47 +0100

    CompraUpdated.java

--- src/model/Compra.java
+++ src/model/Compra.java
@@ -5,146 +5,81 @@ import java.time.LocalDateTime;
 public class Compra {
 
 	private int idCompra;
-
 	private Entrada numEntradas;
-
 	private Cliente cliente;
-
+	private String dniCliente
 	private LocalDateTime fechaCompra;
-
 	private double importe;
-
 	private double descuento;
 
 	
-	public Compra (int idCompra, Entrada numEntradas, Cliente nomCliente, Cliente cliente, LocalDateTime fechaCompra,  double importe, double descuento) {
+	public Compra (int idCompra, Entrada numEntradas, Cliente cliente, String dniCliente, LocalDateTime fechaCompra,  double importe, double descuento) {
 
 		this.idCompra = idCompra;
-
 		this.numEntradas = numEntradas;
-
 		this.cliente = cliente;
-
+		this.dniCliente = dniCliente;
 		this.fechaCompra = fechaCompra;
-
 		this.importe = importe;
-
 		this.descuento = descuento;
 		
 	}
-
-
 	
-
-
-
 	public int getIdCompra() {
 		return idCompra;
 	}
-
-
-
-
-
 
 	public void setIdCompra(int idCompra) {
 		this.idCompra = idCompra;
 	}
 
-
-
-
-
-
 	public Entrada getNumEntradas() {
 		return numEntradas;
 	}
-
-
-
-
-
 
 	public void setNumEntradas(Entrada numEntradas) {
 		this.numEntradas = numEntradas;
 	}
 
-
-
-
-
-
 	public Cliente getCliente() {
 		return cliente;
 	}
-
-
-
-
-
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
 
+	public String getDniCliente() {
+		return dniCliente;
+	}
 
-
-
-
+	public void setDniCliente(String dniCliente) {
+		this.dniCliente = dniCliente;
+	}
 
 	public LocalDateTime getFechaCompra() {
 		return fechaCompra;
 	}
 
-
-
-
-
-
 	public void setFechaCompra(LocalDateTime fechaCompra) {
 		this.fechaCompra = fechaCompra;
 	}
-
-
-
-
-
-
+	
 	public double getImporte() {
 		return importe;
 	}
-
-
-
-
-
-
+	
 	public void setImporte(double importe) {
 		this.importe = importe;
 	}
-
-
-
-
-
 
 	public double getDescuento() {
 		return descuento;
 	}
 
-
-
-
-
-
 	public void setDescuento(double descuento) {
 		this.descuento = descuento;
 	}
-
-
-
-
-
 
 	@Override 
 
@@ -152,9 +87,9 @@ public class Compra {
 		
 		return "ID compra: " + idCompra +
 				" Número entradas: " + numEntradas.getNumPersonas() + 
-				" Cliente: " + cliente.getNombre() + " " + cliente.getApellido() +
-				" DNICliente: " + cliente.getDni() +
-				" Fecha y hora de la compra: " + fechaCompra +
+				" Cliente: " + cliente.getCliente() +
+				" DNICliente: " + cliente.getDniCliente() +
+				" Fecha y hora de la compra: " + fechaCompra.getFechaCompra() +
 				" Importe de la compra: " + importe +
 				" Descuento aplicado: " + descuento;
 				
